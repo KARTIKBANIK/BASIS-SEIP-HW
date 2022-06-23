4278a0ab4366bd98253b12a761284329b450f467
Kartik Banik Shishir <69008307+KARTIKBANIK@users.noreply.github.com>
2022-06-23 18:43:02 +0600

    Create ternary_conditoon_hw2.dart

---
+++ ternary_conditoon_hw2.dart
@@ -0,0 +1,13 @@
+void main(){
+  String name = "SHISHIR";
+  int age = 50;
+  bool isMale = true;
+  
+  print("Your age is : ${age}");
+  
+  age<=25 && name == "SHISHIR" && isMale == true? print('You are child'):
+  age<=45 && name == "SHISHIR" && isMale == true? print('You are young'):
+  age<=65 && name == "SHISHIR" && isMale == true? print('You are old'):
+  age<=85 && name == "SHISHIR" && isMale == true? print('You are very old'):
+  print('Dead');
+}
